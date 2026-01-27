ee8a10ef6f8495af3b43eb1f91eeeaa9a84efb03
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-01-27 00:36:47 -0600

    separate art cards?

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Icon } from "@iconify/react"
 import { toPng } from "html-to-image"
-import { type CSSProperties, useRef } from "react"
+import { type ReactNode, useRef } from "react"
 import { twMerge } from "tailwind-merge"
+
+const tw = String.raw
 
 type Aspect = {
 	name: string
@@ -15,9 +17,7 @@ type Aspect = {
 	actions: string[]
 }
 
-const tw = String.raw
-
-const aspect: Aspect[] = [
+const aspects: Aspect[] = [
 	{
 		name: "Animosity",
 		icon: "mingcute:sword-line",
@@ -26,7 +26,7 @@ const aspect: Aspect[] = [
 		found: "dangerous",
 		effect: "Damage (+1 die)",
 		className: tw`bg-aspects-red text-aspects-red-dark`,
-		actions: ["Strike", "Block", "Push", "Throw", "Endure", "Empower"],
+		actions: ["Strike", "Block", "Push", "Throw", "Endure", "Dominate"],
 	},
 	{
 		name: "Connection",
@@ -36,7 +36,7 @@ const aspect: Aspect[] = [
 		found: "inviting",
 		effect: "Block (1d6)",
 		className: tw`bg-aspects-blue text-aspects-blue-dark`,
-		actions: ["Protect", "Comfort", "Charm", "Read", "Empower", "Restore"],
+		actions: ["Protect", "Comfort", "Charm", "Empower", "Read", "Restore"],
 	},
 	{
 		name: "Freedom",
@@ -46,7 +46,7 @@ const aspect: Aspect[] = [
 		found: "curious",
 		effect: "Reroll 1 die",
 		className: tw`bg-aspects-green text-aspects-green-dark`,
-		actions: ["Dash", "Aim", "Sneak", "Deflect", "Finesse", "Evade"],
+		actions: ["Dash", "Aim", "Finesse", "Sneak", "Deflect", "Evade"],
 	},
 	{
 		name: "Wonder",
@@ -66,131 +66,111 @@ const aspect: Aspect[] = [
 		found: "unsettling",
 		effect: "Evade (2d6)",
 		className: tw`bg-aspects-purple text-aspects-purple-dark`,
-		actions: ["Deceive", "Dominate", "Endure", "Deflect", "Evade", "Finesse"],
+		actions: ["Deceive", "Endure", "Dominate", "Sneak", "Deflect", "Evade"],
 	},
 ]
 
+type Art = {
+	name: string
+	description: string
+	icon: string
+	className: string
+}
+
+const arts: Art[] = [
+	{
+		name: "Ember",
+		description: "Conjure flame and generate heat",
+		icon: "mingcute:flame-line",
+		className: tw`bg-red-800 text-red-200 saturate-60`,
+	},
+	{
+		name: "Flow",
+		description: "Shape and propel natural liquids",
+		icon: "mingcute:drop-line",
+		className: tw`bg-blue-800 text-blue-200 saturate-60`,
+	},
+	{
+		name: "Tempest",
+		description: "Control wind and weather",
+		icon: "mingcute:cloud-windy-line",
+		className: tw`bg-teal-800 text-teal-200 saturate-60`,
+	},
+	{
+		name: "Spark",
+		description: "Summon and direct electrical energy",
+		icon: "mingcute:lightning-line",
+		className: tw`bg-yellow-800 text-yellow-200 saturate-60`,
+	},
+	{
+		name: "Stone",
+		description: "Shift rocks and bend metals",
+		icon: "mingcute:cloud-windy-line",
+		className: tw`bg-neutral-700 text-neutral-200 saturate-60`,
+	},
+	{
+		name: "Verdance",
+		description: "Manipulate or speak with plantlife",
+		icon: "mingcute:leaf-line",
+		className: tw`bg-green-800 text-green-200 saturate-60`,
+	},
+	// {
+	// 	name: "Lumen",
+	// 	description: "Coalesce solid illumination",
+	// 	icon: "mingcute:sun-line",
+	// 	className: tw`bg-yellow-800 text-yellow-200 saturate-60`,
+	// },
+	{
+		name: "Void",
+		description: "Manifest pure featureless darkness",
+		icon: "mingcute:moon-line",
+		className: tw`bg-purple-800 text-purple-200 saturate-30`,
+	},
+	{
+		name: "Frost",
+		description: "Freeze moisture and sculpt ice",
+		icon: "mingcute:snow-line",
+		className: tw`bg-slate-700 text-slate-100`,
+	},
+	{
+		name: "Resonance",
+		description: "Amplify, dampen, and project noise",
+		icon: "mingcute:voice-line",
+		className: tw`bg-cyan-800 text-cyan-200 saturate-60`,
+	},
+	{
+		name: "Psyche",
+		description: "Manipulate the psyche of others",
+		icon: "mingcute:brain-line",
+		className: tw`bg-indigo-800 text-indigo-200 saturate-60`,
+	},
+	{
+		name: "Nebula",
+		description: "Shape the fabric of reality",
+		icon: "mingcute:planet-line",
+		className: tw`bg-stone-800 text-stone-200 saturate-60`,
+	},
+	// {
+	// 	name: "Healing",
+	// 	description: "Mend wounds and restore vitality",
+	// 	icon: "mingcute:heartbeat-2-line",
+	// 	className: tw`bg-pink-800 text-pink-200 saturate-60`,
+	// },
+]
+
+const cardClass = tw`aspect-[2.5/3.5] h-70 overflow-clip rounded-xl border-4`
+
 export function App() {
 	const cardGridRef = useRef<HTMLDivElement>(null)
-
-	const cardClass = tw`aspect-[2.5/3.5] h-70 overflow-clip rounded-xl border-4`
+	const previewRef = useRef<HTMLDivElement>(null)
 
 	return (
-		<div className="py-8">
-			<main className="flex h-dvh flex-col items-center gap-4">
-				<div
-					ref={cardGridRef}
-					className={twMerge(
-						"grid place-content-center gap-2 p-1",
-						"[--card-bg-base-color:0.85_0.23]",
-						"[--card-fg-base-color:0.4_0.1]",
-						"[--icon-col-width:--spacing(10)]",
-					)}
-				>
-					{aspect.map((aspect, aspectIndex) =>
-						[
-							...aspect.actions.sort((a, b) =>
-								a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()),
-							),
-							"Any",
-						]
-							.map((action) => ({
-								name: action,
-								label: `${aspect.name} Skill`,
-								icon: aspect.icon,
-								isArt: false,
-							}))
-							.map((action, actionIndex) => (
-								<div
-									key={`${aspect.name}-${action.name}`}
-									style={
-										{
-											"--row": aspectIndex + 1,
-											"--col": actionIndex + 1,
-										} as CSSProperties
-									}
-									className={twMerge(
-										cardClass,
-										"relative col-(--col) row-(--row) flex flex-col justify-center gap-3 text-shadow-black/10 text-shadow-sm uppercase",
-										aspect.className,
-									)}
-								>
-									<div className="absolute inset-0 bg-stripes opacity-4"></div>
-									<div
-										className="absolute inset-0 bg-linear-to-t from-black/20 via-black/5 data-art:from-white/20 data-art:via-40% data-art:via-white/10"
-										// data-art={action.isArt || undefined}
-									></div>
-
-									<div className="absolute top-0 left-0 p-1.5 opacity-70">
-										<Icon icon={action.icon} className="size-6" />
-									</div>
-									<div className="absolute bottom-0 left-0 p-1.5 opacity-70">
-										<Icon icon={action.icon} className="size-6" />
-									</div>
-									<div className="absolute top-0 right-0 p-1.5 opacity-70">
-										<Icon icon={action.icon} className="size-6" />
-									</div>
-									<div className="absolute right-0 bottom-0 p-1.5 opacity-70">
-										<Icon icon={action.icon} className="size-6" />
-									</div>
-
-									{/* <div className="absolute top-2.5 left-1/2 -translate-x-1/2 font-medium text-[11px] opacity-80">
-										{aspect.name}
-									</div> */}
-									<div className="-translate-x-1/2 -rotate-90 absolute left-3.5 font-medium text-[11px] opacity-80">
-										{action.name}
-									</div>
-									<div className="absolute right-3.5 translate-x-1/2 rotate-90 font-medium text-[11px] opacity-80">
-										{aspect.name}
-									</div>
-
-									{/* <div className="absolute top-2.5 self-center font-medium text-[11px] opacity-80">
-										{aspect.name}
-									</div> */}
-
-									{[
-										// { label: "Aspect", text: aspect.name },
-										{ label: action.label, text: action.name },
-										{ label: "Art", text: aspect.element.join("\n") },
-										{ label: "Find Something", text: aspect.found },
-									].map((row, index) => (
-										<div
-											className="relative flex scale-95 flex-col items-center text-center"
-											key={index}
-										>
-											<h2 className="font-medium text-sm opacity-80">
-												{row.label}
-											</h2>
-											<p className="flex-1 whitespace-pre-line text-balance font-medium text-xl">
-												{row.text}
-											</p>
-										</div>
-									))}
-								</div>
-							)),
-					)}
-
-					<div
-						className={twMerge(
-							cardClass,
-							"-outline-offset-4 relative flex items-center justify-center border-none text-black/45 outline-4 outline-black/60",
-							"bg-linear-to-br from-aspects-purple via-aspects-blue to-aspects-green",
-							// "bg-fuchsia-300",
-						)}
-					>
-						<div className="absolute inset-0 bg-stripes opacity-4"></div>
-						<div className="absolute inset-0 bg-linear-to-t from-black/15"></div>
-						<Icon
-							icon="lucide:pentagon"
-							className="size-32 drop-shadow-black/20 drop-shadow-md"
-						/>
-					</div>
-				</div>
-
-				<button
-					type="button"
-					className="rounded-md bg-aspects-blue px-3 py-2 text-aspects-blue-dark leading-tight transition hover:brightness-80"
-					onClick={() => {
+		<main className="flex h-dvh flex-col items-start gap-4 p-4">
+			<button
+				type="button"
+				className="rounded-md bg-aspects-blue px-3 py-2 text-aspects-blue-dark leading-tight transition hover:brightness-80"
+				onClick={async () => {
+					{
 						const subject = cardGridRef.current as HTMLElement
 						const targetSize = 3000
 
@@ -199,20 +179,219 @@ export function App() {
 							targetSize / subject.clientHeight,
 						)
 
-						toPng(subject, {
+						const dataUrl = await toPng(subject, {
 							canvasWidth: subject.clientWidth * scale,
 							canvasHeight: subject.clientHeight * scale,
-						}).then((dataUrl) => {
-							const link = document.createElement("a")
-							link.download = "aspect-cards.png"
-							link.href = dataUrl
-							link.click()
 						})
-					}}
+						const link = document.createElement("a")
+						link.download = "aspect-cards.png"
+						link.href = dataUrl
+						link.click()
+					}
+					{
+						const subject = previewRef.current as HTMLElement
+						const targetSize = 3000
+
+						const scale = Math.min(
+							targetSize / subject.clientWidth,
+							targetSize / subject.clientHeight,
+						)
+
+						const dataUrl = await toPng(subject, {
+							canvasWidth: subject.clientWidth * scale,
+							canvasHeight: subject.clientHeight * scale,
+						})
+
+						const link = document.createElement("a")
+						link.download = "aspect-cards-preview.png"
+						link.href = dataUrl
+						link.click()
+					}
+				}}
+			>
+				export
+			</button>
+
+			<div className="flex gap-2" ref={previewRef}>
+				{aspects.map((aspect) => (
+					<AspectSkillCard
+						key={aspect.name}
+						aspect={aspect}
+						action={aspect.actions[0]}
+					/>
+				))}
+			</div>
+
+			<div
+				ref={cardGridRef}
+				className="grid grid-cols-[repeat(7,1fr)] place-content-center gap-2"
+			>
+				{aspects.map((aspect, aspectIndex) =>
+					[
+						...aspect.actions.sort((a, b) =>
+							a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()),
+						),
+						"Any",
+					].map((action, actionIndex) => (
+						<div
+							key={`${aspect.name}-${action}`}
+							// style={{
+							// 	gridRow: aspectIndex + 1,
+							// 	gridColumn: actionIndex + 1,
+							// }}
+						>
+							<AspectSkillCard
+								key={aspect.name}
+								aspect={aspect}
+								action={action}
+							/>
+						</div>
+					)),
+				)}
+
+				{arts.map((art) => (
+					<Card
+						key={art.name}
+						className={twMerge(art.className, "gap-5")}
+						icon={art.icon}
+						topLabel={art.name}
+						bottomLabel={art.name}
+						sections={[{ label: "Art", text: art.name }]}
+						description={art.description}
+					/>
+				))}
+
+				<div
+					className={twMerge(
+						cardClass,
+						"-outline-offset-4 relative flex items-center justify-center border-none text-black/45 outline-4 outline-black/60",
+						"bg-linear-to-br from-aspects-purple via-aspects-blue to-aspects-green",
+						// "bg-fuchsia-300",
+					)}
 				>
-					export
-				</button>
-			</main>
+					<div className="absolute inset-0 bg-stripes opacity-4"></div>
+					<div className="absolute inset-0 bg-linear-to-t from-black/15"></div>
+					<Icon
+						icon="lucide:pentagon"
+						className="size-32 drop-shadow-black/20 drop-shadow-md"
+					/>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+function AspectSkillCard({
+	aspect,
+	action,
+}: {
+	aspect: Aspect
+	action: string
+}) {
+	return (
+		<Card
+			className={aspect.className}
+			icon={aspect.icon}
+			topLabel={aspect.name}
+			bottomLabel={aspect.name}
+			leftLabel={action}
+			rightLabel={action}
+			sections={[
+				{ label: "Aspect", text: aspect.name },
+				{ label: `Skill`, text: action },
+				// { label: "Art", text: aspect.element.join("\n") },
+				{ label: "Find Something", text: aspect.found },
+			]}
+		/>
+	)
+}
+
+function Card({
+	className,
+	icon,
+	topLabel,
+	bottomLabel,
+	leftLabel,
+	rightLabel,
+	sections,
+	description,
+}: {
+	className?: string
+	icon?: string
+	topLabel?: string
+	bottomLabel?: string
+	leftLabel?: string
+	rightLabel?: string
+	sections?: { label: string; text: string }[]
+	description?: ReactNode
+}) {
+	return (
+		<div
+			className={twMerge(
+				cardClass,
+				"relative flex flex-col items-center justify-center gap-3 text-center text-shadow-black/10 text-shadow-sm uppercase",
+				className,
+			)}
+		>
+			<div className="absolute inset-0 bg-stripes opacity-4"></div>
+			<div className="absolute inset-0 bg-linear-to-t from-black/20 via-black/5 data-art:from-white/20 data-art:via-40% data-art:via-white/10"></div>
+
+			{icon && (
+				<>
+					<div className="absolute top-0 left-0 p-1.5 opacity-70">
+						<Icon icon={icon} className="size-6" />
+					</div>
+					<div className="absolute bottom-0 left-0 p-1.5 opacity-70">
+						<Icon icon={icon} className="size-6" />
+					</div>
+					<div className="absolute top-0 right-0 p-1.5 opacity-70">
+						<Icon icon={icon} className="size-6" />
+					</div>
+					<div className="absolute right-0 bottom-0 p-1.5 opacity-70">
+						<Icon icon={icon} className="size-6" />
+					</div>
+				</>
+			)}
+
+			{topLabel && (
+				<div className="-translate-x-1/2 absolute top-2.5 left-1/2 font-medium text-[11px] opacity-80">
+					{topLabel}
+				</div>
+			)}
+			{bottomLabel && (
+				<div className="-translate-x-1/2 absolute bottom-2.5 left-1/2 font-medium text-[11px] opacity-80">
+					{bottomLabel}
+				</div>
+			)}
+
+			{leftLabel && (
+				<div className="-translate-x-1/2 -rotate-90 absolute left-3.5 font-medium text-[11px] opacity-80">
+					{leftLabel}
+				</div>
+			)}
+			{rightLabel && (
+				<div className="absolute right-3.5 translate-x-1/2 rotate-90 font-medium text-[11px] opacity-80">
+					{rightLabel}
+				</div>
+			)}
+
+			{sections?.map((section, index) => (
+				<div
+					className="relative flex scale-95 flex-col items-center text-center"
+					key={index}
+				>
+					<h2 className="font-medium text-sm opacity-80">{section.label}</h2>
+					<p className="flex-1 whitespace-pre-line text-balance font-medium text-xl">
+						{section.text}
+					</p>
+				</div>
+			))}
+
+			{description && (
+				<p className="flex h-16 items-center text-balance px-5 leading-tight">
+					{description}
+				</p>
+			)}
 		</div>
 	)
 }
